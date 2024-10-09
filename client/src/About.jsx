c0bffd4ef17651fe9f507790ccf49ae2a18bd7ec
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-10-08 21:08:25 -0500

    wrote about me, may change later

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -1,3 +1,22 @@
 export default function About() {
-    
+	return (
+		<>
+			<h1>About me</h1>
+			<p>
+				My Web Development journey began in August 2022 when I enrolled
+				into FreeCodeCamps Responsive Web Design certification. My first
+				project was to code a cat website in html, and at this point I
+				knew I was hooked. I completed this course, and afterwards found
+				The Odin Project, the curriculum I'm currently utilizing to hone
+				my skills in Web Development. <br />
+				<br />I love coding. The aha feeling I get when I solve a tricky
+				bug, the satisfaction I feel when I show friends my new
+				projects, or the feeling of accomplishment when I experience a
+				breakthrough all draw me to programming. <br />
+				<br />
+				When I'm not coding, I enjoy playing tennis, pickleball, or
+				chess.
+			</p>
+		</>
+	);
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,3 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
-    
+	const navigate = useNavigate();
+
+	const handleClick = () => {
+		navigate("/contact");
+	};
+
+	return (
+		<>
+			<h1 className="title">
+				Hello, world! I&apos;m Jonathan Waddell. And I do web
+				development.
+			</h1>
+			<button onClick={handleClick}>Contact me</button>
+		</>
+	);
 }
